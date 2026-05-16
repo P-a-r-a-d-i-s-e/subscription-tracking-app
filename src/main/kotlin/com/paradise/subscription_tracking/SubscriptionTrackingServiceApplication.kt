f990964f9ebd6780c62d65fe44249fc9f8f237f8
paradise <saymonsik@gmail.com>
2026-05-16 20:13:16 +0400

    add @EnableScheduling in SubscriptionTrackingApplication

--- src/main/kotlin/com/paradise/subscription_tracking/SubscriptionTrackingServiceApplication.kt
+++ src/main/kotlin/com/paradise/subscription_tracking/SubscriptionTrackingServiceApplication.kt
@@ -2,10 +2,12 @@ package com.paradise.subscription_tracking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class SubscriptionTrackingApplication
 
 fun main(args: Array<String>) {
-	runApplication<SubscriptionTrackingApplication>(*args)
+    runApplication<SubscriptionTrackingApplication>(*args)
 }
