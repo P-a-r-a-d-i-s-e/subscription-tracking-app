747804a486faa2fe6aa3e7226fe9a25b3ec963fb
paradise <saymonsik@gmail.com>
2026-05-17 12:58:18 +0400

    change postgres version in TestcontainersConfiguration

--- src/test/kotlin/com/paradise/subscription_tracking/SubscriptionTrackingServiceApplicationTests.kt
+++ src/test/kotlin/com/paradise/subscription_tracking/SubscriptionTrackingServiceApplicationTests.kt
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Import
 @SpringBootTest
 class SubscriptionTrackingApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
